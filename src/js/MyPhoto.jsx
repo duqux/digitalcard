f271ff96ed56f68e96088c988e6179596a68b232
joeygoh <joeygoh@ecquaria.com>
2022-07-27 15:18:50 +0800

    Changing dist

--- src/js/MyPhoto.jsx
+++
@@ -1,9 +0,0 @@
-import React from "react"
-
-export default function MyPhoto(){
-    return (
-        <div>
-            <img src="src/assets/photo.jfif" className="photo-image"/>        
-        </div>
-    )
-}
