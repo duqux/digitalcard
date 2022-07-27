ed5fed302ce27a43a1ea3a31bf8992466524ef63
joeygoh <joeygoh@ecquaria.com>
2022-07-27 15:16:02 +0800

    deploy image changes

--- src/js/MyPhoto.jsx
+++ src/js/MyPhoto.jsx
@@ -3,7 +3,7 @@ import React from "react"
 export default function MyPhoto(){
     return (
         <div>
-            <img src="./src/assets/photo.jfif" className="photo-image"/>        
+            <img src="src/assets/photo.jfif" className="photo-image"/>        
         </div>
     )
 }
