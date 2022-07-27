fe435aaf43d400abadfb49a6f8f3b36a5e35ff78
joeygoh <joeygoh@ecquaria.com>
2022-07-27 17:05:26 +0800

    Changing URL

--- src/js/Footer.jsx
+++ src/js/Footer.jsx
@@ -7,22 +7,22 @@ import github from '/src/assets/GitHub Icon.png'
 export default function Footer(){
     return (
         <div>
-            <a href="https://www.twitter.com" target="_blank">
+            <a href="https://www.twitter.com/caratothemoon" target="_blank">
             <button className="social-icon">
             <img src={twitter}/>        
                 </button>
             </a>
-            <a href="https://www.facebook.com" target="_blank">
+            <a href="https://www.facebook.com/duqux" target="_blank">
             <button className="social-icon">
             <img src={facebook}/>        
                 </button>
             </a>
-            <a href="https://www.instagram.com" target="_blank">
+            <a href="https://www.instagram.com/jgzuer" target="_blank">
             <button className="social-icon">
             <img src={instagram}/>        
                 </button>
             </a>
-            <a href="https://www.github.com" target="_blank">
+            <a href="https://github.com/duqux/digitalcard" target="_blank">
             <button className="social-icon">
             <img src={github}/>        
                 </button>
