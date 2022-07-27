a093de3b6c5d33282bb66ea81d11b0d96e35224b
joeygoh <joeygoh@ecquaria.com>
2022-07-27 16:53:49 +0800

    push image changes

--- src/js/MySocials.jsx
+++ src/js/MySocials.jsx
@@ -1,18 +1,19 @@
 import React from "react"
-
+import emailimg from '/src/assets/Icon.png'
+import linkedinimg from 'src/assets/Vector.png'
 
 export default function MySocials(){
     return (
         <div className="buttongroup">
             <a href="mailto:joeygoh@ecquaria.com" target="_blank">
             <button className="emailbutton">
-            <img src="./src/assets/Icon.png"className="icon-email"/>        
+            <img src={emailimg} className="icon-email"/>        
                 Email
                 </button>
             </a>
             <a href="https://www.linkedin.com" target="_blank">
             <button className="linkedinbutton">
-            <img src="./src/assets/Vector.png"className="icon-linkedin"/>   
+            <img src={linkedinimg} className="icon-linkedin"/>   
                 LinkedIn
                 </button>
             </a>
